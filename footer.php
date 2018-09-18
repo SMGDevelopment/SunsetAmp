4a1eeaa5ac2fe6c309e54c5eedfa281b5d8484c7
Will Alexander <walexander@mco.com>
2018-09-18 09:58:39 -0400

    make galleries work semi-accurately

--- footer.php
+++ footer.php
@@ -2,4 +2,12 @@
 <footer class="footer container">
 	<?php amp_non_amp_link(); ?>
 </footer>
+
+    <amp-sticky-ad layout="nodisplay">
+    <amp-ad width="320"
+      height="50"
+      type="doubleclick"
+      data-slot="/35096353/amptesting/formats/sticky">
+    </amp-ad>
+  </amp-sticky-ad>
 <?php amp_footer_core(); ?>
--- single.php
+++ single.php
@@ -3,11 +3,6 @@
 <?php amp_featured_image();?>
 <?php amp_content(); ?>
 <?php amp_post_pagination();?>
-<?php amp_author_box(); ?>
-<?php amp_social(array('twitter'));?>
-<?php amp_categories_list();?>
-<?php amp_tags_list();?>
-<?php amp_comments();?>
 <?php amp_post_navigation();?>
 <?php amp_related_posts(); ?>
 <?php amp_footer()?>
